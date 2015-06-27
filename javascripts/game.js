d808b03724a0c8ee5fbfc2ed7d9b09e5f5b7a813
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2015-06-27 16:11:13 +0100

    Gameplay - work in progress

--- javascripts/game.js
+++ javascripts/game.js
@@ -20,36 +20,102 @@ let cardSuits = ["clubs", "diamonds", "hearts", "spades"];
 let deckCards = _.flatten(cardRanks.map((rank) => {
 	return cardSuits.map((suit) => {
 		return {
-			name: `${rank.name}_of_${suit}"`,
+			name: `${rank.name}_of_${suit}`,
 			score: rank.score
 		};
-	})
+	});
 }));
 
 // Create the final deck using 6 card deck and shuffle the cards
-let deck = _.chain(_.range(0, 6))
+var deck = _.chain(_.range(0, 6))
 	.map(() => deckCards)
 	.flatten()
 	.shuffle()
-	.value()
+	.value();
 
+var currentPlayer = 0
 var playerGameView = {
 	players: [
-		{ name: "Dealer" },
-		{ name: "Player1" },
-		{ name: "Player2" },
-		{ name: "Player3" },
-		{ name: "Player4" }
+		{ type: "dealer", name: "Dealer", hand: [], currentTurn: "", score: 0 },
+		{ type: "player", name: "Player 1", number: 1, hand: [], currentTurn: "", score: 0 },
+		{ type: "player", name: "Player 2", number: 2, hand: [], currentTurn: "", score: 0 },
+		{ type: "player", name: "Player 3", number: 3, hand: [], currentTurn: "", score: 0 },
+		{ type: "player", name: "Player 4", number: 4, hand: [], currentTurn: "", score: 0 }
 	]
 };
 
-var refreshGame = () => {
+let refreshGameView = () => {
 	let playerGameTmpl = document.getElementById("playerGameTmpl").innerHTML;
 	document.getElementById("table").innerHTML = Mustache.render(playerGameTmpl, playerGameView);
 };
 
+let changeTurn = (player) => {
+	currentPlayer++
+	playerGameView.players.forEach((player) => {
+		player.currentTurn = ""
+	});
+	player.currentTurn = "currentTurn";
+}
+
+let newRound = () => {
+	currentPlayer = 0
+
+	// Select all players (exclude dealer)
+	let players = _.filter(playerGameView.players, (player) => player.type == "player");
+
+	// Give two cards to every player
+	players.forEach((player) => {
+		_(2).times(() => {
+			hit(player.name);
+		});
+	});
+
+	// Set first player's turn
+	changeTurn(_.first(players))
+}
+
+let newRoundEvent = () => {
+	newRound()
+	refreshGameView();
+};
+
+let hit = (playerName) => {
+
+	// Take first card in the deck
+	let card = _.first(deck);
+	deck = _.tail(deck);
+
+	// Add the card to the player's hand
+	var player = _.findWhere(playerGameView.players, { name: playerName });
+	player.hand.push(card);
+
+	player.score += card.score;
+
+};
+
+let hitEvent = (playerName) => {
+	hit(playerName);
+	refreshGameView();
+}
+
+let stick = (playerName) => {
+	let player = _.findWhere(playerGameView.players, { number: currentPlayer + 1 });
+	if (player) {
+		changeTurn(player);
+	}
+	else {
+		let dealer = _.findWhere(playerGameView.players, { type: "dealer" });
+		changeTurn(dealer);
+	}
+};
+
+let stickEvent = (playerName) => {
+	stick(playerName);
+	refreshGameView();
+}
+
 let gameLoop = () => {
-	refreshGame();
+	refreshGameView();
 };
 
 window.onload = () => {
